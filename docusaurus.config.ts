ddb280329330b0a98d67eaa68ac77a552a9e5cbd
Mweya <mweya.ruider@iits-consulting.de>
2025-02-27 16:18:03 +0100

    feat(ci): Add ci

---
+++ docusaurus.config.ts
@@ -0,0 +1,85 @@
+// Themes Config
+import {themes} from "prism-react-renderer";
+
+const theme = themes.github;
+const darkTheme = themes.oneLight;
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: "terraform provider otc-marketplace | Documentation",
+    tagline: "Provider API reference",
+    favicon: "https://usercontent.one/wp/iits-consulting.de/wp-content/uploads/2024/03/cropped-favicon-iits-2024-270x270.png",
+    url: "https://iits.de",
+    baseUrl: "/",
+    onBrokenLinks: 'ignore',
+    onBrokenMarkdownLinks: 'warn',
+    organizationName: "iits",
+    projectName: "Docs",
+    plugins: [
+        [
+            '@docusaurus/plugin-client-redirects',
+            {
+                toExtensions: ['html'],
+                redirects: [
+                    {
+                        to: '/docs',
+                        from: ['/'],
+                    },
+                ],
+            }
+        ],
+        'docusaurus-plugin-sass',
+    ],
+    scripts: [
+        {
+            src: "./src/js/wiki-version.js",
+        },
+    ],
+    presets: [
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        [
+            '@docusaurus/preset-classic',
+            ({
+                docs: {
+                    routeBasePath: '/docs',
+                    editUrl: 'https://gitlab.iits.tech/private/otc-marketplace-provider/-/blob/main/terraform-provider-otc-marketplace/',
+                    showLastUpdateAuthor: true,
+                    showLastUpdateTime: true,
+                    editLocalizedFiles: true,
+                    editCurrentVersion: true,
+                    versions: {
+                        current: {
+                            label: 'v0.1.0',
+                        },
+                    },
+                    lastVersion: 'current',
+                },
+                blog: {
+                    showReadingTime: true,
+                },
+                theme: {},
+            }),
+        ],
+    ],
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    themeConfig: {
+        metadata: [{
+            name: 'keywords',
+            content: 'marketplace, otc, terraform, provider'
+        }],
+        colorMode: {
+            defaultMode: 'dark',
+            disableSwitch: false,
+            respectPrefersColorScheme: true,
+        },
+        prism: {
+            theme: theme,
+            darkTheme: darkTheme,
+            additionalLanguages: [
+                "go",
+            ],
+        },
+    },
+};
+
+export default config;
