539715cc9ea0177760043e1cf993c1f29038cfdd
Mweya <mweya.ruider@iits-consulting.de>
2025-03-05 16:22:22 +0100

    fix(ci): Change basepath

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config = {
                 toExtensions: ['html'],
                 redirects: [
                     {
-                        to: '/docs',
+                        to: '/terraform-provider-otc-marketplace/docs',
                         from: ['/'],
                     },
                 ],
